77e37b7d5a7874f48fa41bd72b21b74d2e5e49ab
Vimukthi Malinda <114574555+Vimuwa96@users.noreply.github.com>
2024-05-02 16:11:57 +0530

    Add files via upload

---
+++ js/tablink.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Set the first tab as active
+  document.querySelector(".tablinks").classList.add("active");
+});
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+$(".your-class").slick({
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
